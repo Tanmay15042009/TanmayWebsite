00f1d4122c5ae18dc8b4abc2d62ad58e715f3334
Tanmay15042009 <tanmayrastogi15@gmail.com>
2022-01-14 12:05:20 +0530

    first commit

---
+++ myjavascript.js
@@ -0,0 +1,28 @@
+setTimeout (function () {document.getElementById('p1').innerHTML="This is my First Java Script"; }, 3000)
+var i;
+i = 6476576575467;
+console.log (i) ;
+var n = 223323233221123;
+console.log (n) ;
+var tx = "The number below is the addition of the first number and 2nd number";
+console.log (tx) ;
+var t = i+n;
+console.log (t) ;
+console.log (typeof (i));
+var e ="Tanmay";
+console.log (typeof (e));
+console.log (e);
+var g =123;
+console.log (g);
+var f =145
+console.log (f);
+var k = g*f;
+console.log (k);
+console.log (i+g);
+console.log (1212324/2235);
+var tanmay = 42134231324;
+console.log (tanmay);
+var gh = 70000000;
+console.log(gh);
+var j = 500000000000000;
+console.log (j);
